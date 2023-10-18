b1c18c3c60e912f25d79678bf3b5370a5c504954
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-18 11:42:20 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+package Conditional;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=79;
+		int num2=78;
+		char ch='/';
+		/*char ch1='-';
+		char ch2='*';
+		char ch3='/';*/
+	if(ch=='+') {
+		System.out.println(num1+num2);
+	}
+	else if(ch=='-') {
+		System.out.println(num1-num2);
+	}
+	else if(ch=='*') {
+		
+		System.out.println(num1*num2);
+	}
+	else if(ch=='/') {
+		System.out.println(num1/num2);
+	}
+	else {
+		System.out.println("bye");
+	}
+
+	}
+
+}
