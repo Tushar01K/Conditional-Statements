fe3b8b576a0c4e52238d63feecaf3ab3eb985a69
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-19 00:24:50 +0530

    Add files via upload

---
+++ fibonacciseries.java
@@ -0,0 +1,19 @@
+package Conditional;
+
+public class fibonacciseries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 1;
+		System.out.println(a);
+		System.out.println(b);
+		for (int i = 1; i <= 10; i++) {
+			int c = a + b;
+			System.out.println(c);
+			a = b;
+			b = c;
+		}
+	}
+
+}
