cd328538c11d728e2192e533aaba097d69fa8c81
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-19 00:28:37 +0530

    Add files via upload

---
+++ Maxmin.java
@@ -0,0 +1,22 @@
+package Conditional;
+
+public class Maxmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=45;
+		int num2=98;
+		if(num1>num2) {
+			System.out.println("num1 is max and num2 is min");
+		}
+		else if(num2>num1) {
+			System.out.println("num2 is max and num1 is min");
+		}
+		else {
+			System.out.println("default ");
+			
+		}
+
+	}
+
+}
