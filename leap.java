99d8b26f624db4d3b3ca75f771739a54fe5887f7
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-19 00:28:07 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,20 @@
+package Conditional;
+
+public class leap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=2023;
+		if(year%2000==0) {
+			System.out.println("leap");
+			
+		}
+		else {
+			System.out.println(" not leap");
+			
+		}
+		}
+
+	}
+
+
