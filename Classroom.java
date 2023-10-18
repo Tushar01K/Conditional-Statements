a7e443a0af3ce632fbef3ecd5aee4b08cb8671c0
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-18 11:43:46 +0530

    Add files via upload

---
+++ Classroom.java
@@ -0,0 +1,29 @@
+package Conditional;
+
+public class Classroom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rn=235;
+		if(rn<=100) {
+			
+			System.out.println("enter in cr-1");
+		}
+		else if(rn<=200) {
+			System.out.println("enter in CR-2");
+			
+		}
+		else if(rn<=250) {
+		
+		System.out.println("enter in CR-3");
+	}
+		else {
+			System.out.println("baher ja baba");
+		}
+		
+		
+	
+
+	}
+
+}
