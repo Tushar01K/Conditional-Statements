ddf647dd59f7441223f47c7575f670befe6e0080
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-18 11:47:26 +0530

    Add files via upload

---
+++ endingwithspecificnumber.java
@@ -0,0 +1,18 @@
+package Conditional;
+
+public class endingwithspecificnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int endingnum=7;
+for(int i=100;i<=200;i++) {
+	if(i%10==endingnum) {
+		System.out.println(i);
+	}
+	
+
+		}
+	}
+	
+
+	}
