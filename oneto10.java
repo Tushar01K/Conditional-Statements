195ffc76c0789616961b41a589f80fedcaa3c76c
Tushar01K <110174526+Tushar01K@users.noreply.github.com>
2023-10-27 18:10:30 +0530

    Add files via upload

---
+++ oneto10.java
@@ -0,0 +1,20 @@
+package Conditional;
+
+public class oneto10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		int num=2;
+		while(i<=10) { 
+			
+			System.out.println(num);
+			num=num+2;
+			i++;
+		}
+		
+		
+
+	}
+
+}
